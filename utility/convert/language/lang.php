8a3b44932af593602a75c0f5792c2a034174d706
discuzvn <73950054+discuzvnnet@users.noreply.github.com>
2021-02-09 13:57:06 +0700

    update 1

---
+++ utility/convert/language/lang.php
@@ -0,0 +1,61 @@
+<?php
+
+$language = array(
+
+	'config_dbhost' => 'Máy chủ cơ sở dữ liệu',
+	'config_dbuser' => 'Người dùng cơ sở dữ liệu',
+	'config_dbpw' => 'Mật khẩu cơ sở dữ liệu',
+	'config_dbname' => 'Cơ sở dữ liệu',
+	'config_tablepre' => 'Tiền tố bảng dữ liệu',
+	'config_dbcharset' => 'Bộ ký tự bảng dữ liệu (tùy chọn)',
+	'config_pconnect' => 'Kết nối liên tục',
+
+	'config_type_source' => 'Cài đặt máy chủ nguồn dữ liệu (phiên bản gốc của cơ sở dữ liệu)',
+	'config_type_target' => 'Cài đặt máy chủ đích (Cơ sở dữ liệu của Discuz! X đã được cài đặt đúng cách)',
+	'config_type_ucenter' => 'Cài đặt máy chủ UCenter (cơ sở dữ liệu UCenter đã được cài đặt đúng cách)',
+
+	'config_type_target_comment' => 'Lưu ý: Dữ liệu trong cơ sở dữ liệu đích sẽ được thay thế hoặc cập nhật. Nếu chứa nội dung quan trọng, vui lòng sao lưu trước',
+	'config_write_error' => 'Dữ liệu / thư mục của chương trình chuyển đổi không thể ghi, vui lòng đảm bảo rằng thư mục này có quyền có thể ghi và đặt thuộc tính 777',
+
+	'config_save' => 'Lưu cài đặt máy chủ',
+	'config_usergroup' => 'Cấu hình nhóm người dùng',
+	'config_from_usergroup' => 'Nhóm người dùng nguồn',
+	'config_target_usergroup' => 'Nhóm người dùng mục tiêu',
+	'config_extcredits' => 'Cấu hình điểm',
+	'config_credit' => 'Tín dụng',
+	'config_from_credit' => 'Điểm nguồn',
+	'config_target_credit' => 'Điểm mục tiêu',
+	'config_experience' => 'Kinh nghiệm',
+	'config_convert_forum' => 'Cấu hình chuyển đổi dữ liệu',
+	'config_from_data' => 'Nguồn dữ liệu',
+	'config_target_forum' => 'Mục tiêu',
+	'config_poll' => 'Biểu quyết',
+	'config_event' => 'Sự kiện',
+	'config_auto_create' => 'Được tạo tự động',
+	'config_convert' => 'Lưu cấu hình chuyển đổi',
+
+	'submit' => 'Gửi đi',
+
+	'message_return' => 'Quay lại bước trước',
+	'message_stop' => 'Ngừng chạy',
+	'message_not_enabled_extcredit' => 'Ít nhất một điểm phải được bật trong hệ thống mới, nếu không, không thể thực hiện chuyển đổi tiếp theo',
+
+	'ok' => '&nbsp;Xác&nbsp;&nbsp;nhận&nbsp;',
+	'cancel' => '&nbsp;Hủy&nbsp;bỏ&nbsp;',
+	'return' => 'Xin vui lòng trở lại',
+	'tips' => 'Lời khuyên',
+	'tips_pconnect' => 'Lưu ý: Nếu cơ sở dữ liệu nguồn và cơ sở dữ liệu đích nằm trên cùng một máy chủ, mục này phải được đặt thành 0, nếu không, mục này được đặt thành 1.',
+
+	'mysql_config_error' => 'Cấu hình không được để trống',
+	'mysql_connect_error' => 'Kết nối thất bại',
+
+	'config_success' => 'Cấu hình máy chủ thành công, bây giờ hãy chuyển sang bước tiếp theo',
+
+	'setting_tips' => '<li>Việc sửa đổi cài đặt chương trình chuyển đổi có thể làm giảm hiệu quả chuyển đổi của bạn hoặc không thể hoàn thành quá trình chuyển đổi bình thường, vì vậy hãy cẩn thận khi sửa đổi</li>',
+
+	'mysql_connect_error_1' => 'Lỗi kết nối cơ sở dữ liệu, vui lòng kiểm tra xem mật khẩu và tài khoản cơ sở dữ liệu có đúng không',
+	'mysql_connect_error_2' => 'Lỗi kiểm tra bảng dữ liệu, bạn có thể không điền đúng "tiền tố bảng dữ liệu" hoặc bạn chưa cài đặt phiên bản này của chương trình',
+
+
+
+);
